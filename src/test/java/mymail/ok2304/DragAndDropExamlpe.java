b4979192aa978ad5c1d88a705ce122a5645f093e
Olga <49266934+olgakos@users.noreply.github.com>
2022-02-21 19:43:38 +0300

    Add github search in wiki pages menu-bar test

---
+++ src/test/java/mymail/ok2304/DragAndDropExamlpe.java
@@ -0,0 +1,28 @@
+package mymail.ok2304;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+//для @BeforeAll
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Configuration.browserSize;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDropExamlpe {
+        @BeforeAll
+        static void openBrowser() {
+            baseUrl = "https://the-internet.herokuapp.com";
+            browserSize = "1920x1080";
+        }
+
+        @Test
+        void dragDropTest(){
+            open("/drag_and_drop");
+            $("#column-a").shouldHave(text("A"));
+            $("#column-a").dragAndDropTo("#column-b");
+            $("#column-a").shouldHave(text("B"));
+        }
+}
