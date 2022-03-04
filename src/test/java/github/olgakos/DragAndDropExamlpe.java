3b9f88fe156516479a72bd481a3a4fdc2d018210
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-03-04 19:20:12 +0300

    Moved DragAndDrop start actions to TestBaseTheInternet and made it a parent class

--- src/test/java/github/olgakos/DragAndDropExamlpe.java
+++ src/test/java/github/olgakos/DragAndDropExamlpe.java
@@ -1,20 +1,11 @@
 package github.olgakos;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.Configuration.browserSize;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class DragAndDropExamlpe {
-        @BeforeAll
-        static void openBrowser() {
-            baseUrl = "https://the-internet.herokuapp.com";
-            browserSize = "1920x1080";
-        }
-
+public class DragAndDropExamlpe extends TestBaseTheInternet {
         @Test
         void dragDropTest(){
             open("/drag_and_drop");
